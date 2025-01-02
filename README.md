c143573cd9097d7c01196f30fafe7680f9701cbe
Jinesh87 <zodiacaus@gmail.com>
2025-01-02 16:25:12 +0530

    Taking Photo on Android

--- README.md
+++ README.md
@@ -16,7 +16,6 @@ A React Native application for managing and showcasing your favorite places. Thi
 - **[react-native-screens](https://reactnavigation.org/docs/getting-started#installing-dependencies-into-an-expo-managed-project)**: Installing dependencies into an Expo managed project.
 - **[react-native-safe-area-context](https://reactnavigation.org/docs/getting-started#installing-dependencies-into-an-expo-managed-project/)**: Installing dependencies into an Expo managed project.
 - **[@react-navigation/native-stack](https://reactnavigation.org/docs/native-stack-navigator#installation)**: Stack Navigator provides a way for your app to transition between screens where each new screen is placed on top of a stack.
-- **[@react-navigation/bottom-tabs](https://reactnavigation.org/docs/bottom-tab-navigator)**:A simple tab bar on the bottom of the screen that lets you switch between different routes.
 
 
 ## Installation and Setup Instructions 
--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -1,9 +1,48 @@
-import { View,Text, StyleSheet } from "react-native";
+import { useState } from "react";
+import { View, Text, TextInput, StyleSheet, ScrollView } from "react-native";
+import ImagePicker from "./ImagePicker";
+import { Colors } from "../../constants/colors";
+
+
 function PlaceForm() {
+    const [enteredTitle, setEnteredTitle] = useState('');
+
+    function changeTitleHandler(enteredText) {
+        setEnteredTitle(enteredText);
+    }
+
     return (
-        <View>
-            <Text>The Place Form</Text>
-        </View>
+        <ScrollView>
+            <View>
+                <Text style={styles.label}>Title</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={changeTitleHandler}
+                    value={enteredTitle}
+                />
+            </View>
+            <ImagePicker />
+        </ScrollView>
     );
 }
 export default PlaceForm;
+const styles = StyleSheet.create({
+    form: {
+      flex: 1,
+      padding: 24,
+    },
+    label: {
+      fontWeight: 'bold',
+      marginBottom: 4,
+      color: Colors.primary500,
+    },
+    input: {
+      marginVertical: 8,
+      paddingHorizontal: 4,
+      paddingVertical: 8,
+      fontSize: 16,
+      borderBottomColor: Colors.primary700,
+      borderBottomWidth: 2,
+      backgroundColor: Colors.primary100,
+    },
+  });
