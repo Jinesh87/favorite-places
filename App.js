874771eb9e7c2013deec01905d9689fbc33962fd
Jinesh87 <zodiacaus@gmail.com>
2025-01-02 15:50:34 +0530

    Global Colors and Styling

--- App.js
+++ App.js
@@ -1,35 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
 import IconButton from './UI/IconButton';
+import { Colors } from './constants/colors';
+
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="auto" />
+      <StatusBar style="dark" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: Colors.primary500 },
+            headerTintColor: Colors.gray700,
+            contentStyle: { backgroundColor: Colors.gray700 },
+          }}
+        >
           <Stack.Screen
             name="AllPlaces"
             component={AllPlaces}
             options={({ navigation }) => ({
+              title: 'Your Favorite Places',
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon="add"
-                  size={24}
+                  size={40}
                   color={tintColor}
                   onPress={() => navigation.navigate('AddPlace')}
                 />
               ),
             })}
           />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen name="AddPlace" component={AddPlace} options={{
+              title: 'Add a new Place',
+            }} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
-
   );
 }
--- components/Places/PlacesList.js
+++ components/Places/PlacesList.js
@@ -1,5 +1,6 @@
 import { FlatList, StyleSheet, View, Text } from "react-native";
 import PlaceItem from "./PlaceItem";
+import { Colors } from "../../constants/colors";
 
 function PlacesList({ places }) {
     if (!places || places.length === 0) {
@@ -20,11 +21,12 @@ function PlacesList({ places }) {
 export default PlacesList;
 const styles = StyleSheet.create({
     fallbackContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
     },
     fallbackText: {
-        fontSize: 16,
+      fontSize: 16,
+      color: Colors.primary200
     },
-});
+  });
